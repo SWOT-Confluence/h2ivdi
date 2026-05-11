60911a14aef546418b5efe51edd0f46d96e5c315
mike-gangl <59702631+mike-gangl@users.noreply.github.com>
2026-05-10 19:57:52 -0700

    Update confluence-hivdi.tf

--- terraform/modules/hivdi/confluence-hivdi.tf
+++ terraform/modules/hivdi/confluence-hivdi.tf
@@ -4,7 +4,7 @@ resource "aws_batch_job_definition" "generate_batch_jd_hivdi" {
   type                  = "container"
   container_properties  = <<CONTAINER_PROPERTIES
   {
-    "image": "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-hivdi",
+    "image": "${local.account_id}.dkr.ecr.us-west-2.amazonaws.com/${var.prefix}-h2ivdi:${var.image_tag}",
     "executionRoleArn": "${data.aws_iam_role.exe_role.arn}",
     "jobRoleArn": "${data.aws_iam_role.job_role.arn}",
     "fargatePlatformConfiguration": { "platformVersion": "LATEST" },
